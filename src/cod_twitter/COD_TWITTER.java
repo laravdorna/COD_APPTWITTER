6b9316467c2efeae2266f4907b672a240c37b36c
lvazquezdorna <lvazquezdorna@nautilus17.danielcastelao.org>
2019-03-26 09:20:40 +0100

    1.Init

---
+++ src/cod_twitter/COD_TWITTER.java
@@ -0,0 +1,22 @@
+/*
+ * pROYECTO COD_APPTWITTER para usar la twitter4J 
+ * https://github.com/Twitter4J/Twitter4J
+
+ */
+
+package cod_twitter;
+
+/**
+ *
+ * @author Lara Vazquez Dorna
+ */
+public class COD_TWITTER {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
